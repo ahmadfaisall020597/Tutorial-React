f4f804289414d70d44b6fbc27a33176f2f42c09e
Ahmad  Faisal <ahmad.faisal@berca.co.id>
2023-02-09 08:21:43 +0700

    First React Component

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+/* 
+Task React
+My first react project
+  • Call My Name
+  • My Address
+*/
+
+function MainBody() {
+  return (
+    <div>
+      <h1>My first react project</h1>
+      <ul>
+        <li>Call My Name</li>
+        <li>My address</li>
+      </ul>
+    </div>
+  )
+}
+
+function Dummy() {
+  return (
+    <div>
+      <h1>Biodata</h1>
+      <ul>
+        <li>
+          <span>Name : </span><span>Faisal</span>
+        </li>
+        <li><span>Address : </span> <span>Kemang</span></li>
+      </ul>
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <div>Hello World</div>
+  <div>
+    <MainBody />
+    <Dummy />
+  </div>
 );
