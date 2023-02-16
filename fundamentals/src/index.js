1d5030d9abf817c9f157766651ec9cbe5d222cde
Ahmad  Faisal <ahmad.faisal@berca.co.id>
2023-02-16 09:13:38 +0700

    Student Component

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -41,15 +41,39 @@ function MainBody() {
 
   return (
     <div>
-      <h3>In this, we will learn { whatWeWillLearn } by building faisal</h3>
-      <p>Total Lecture = { lectureCount }</p>
+      <h3>In this, we will learn {whatWeWillLearn} by building faisal</h3>
+      <p>Total Lecture = {lectureCount}</p>
       <ul>
         <li>Basic Foundation</li>
         <li>Functional and Class Components</li>
       </ul>
       <div>
-        Enter Task : {" "} 
+        Enter Task : {" "}
         <input maxLength={8} readOnly={false} placeholder="Enter your input"></input>
+      </div>
+    </div>
+  )
+}
+
+function Students() {
+  const fullName = "Ahmad Faisal"
+  const programmingExp = 3;
+  return (
+    <div className='container p-4'>
+      <div className='row'>
+        Students Enrolled
+      </div>
+      <div className='row border'>
+        <div className='col-1'>
+          <img src={`https://ui-avatars.com/api/?name=${fullName}`}
+            className='w-100'>
+          </img>
+        </div>
+        <div className='col-10'>
+          {fullName}
+          <br />
+          Programming Experience {programmingExp} years
+        </div>
       </div>
     </div>
   )
@@ -68,6 +92,7 @@ root.render(
   <div>
     <Header />
     <MainBody />
+    <Students />
     <Footer />
   </div>
 );
