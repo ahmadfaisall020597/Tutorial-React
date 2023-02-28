16bb665c9f15bb57fc52bffa461f490f4d305c8f
Ahmad  Faisal <ahmad.faisal@berca.co.id>
2023-02-28 11:42:51 +0700

    Random play and Reset Button

--- count-pedia/src/Header.jsx
+++ count-pedia/src/Header.jsx
@@ -2,9 +2,9 @@ import logo from "../src/images/react.png";
 
 export default function Header() {
     return (
-        <div className="px-2 py-3" style={{borderBottom: "1px solid #777"}}>
-            <img src={logo} alt="" style={{height: "35px", verticalAlign: "top"}} />
-            <span className="h2 py-2 px-2 text-white-50">React Course - CountOPedia</span>
+        <div className="py-2 pl-2" style={{ borderBottom: "1px solid #777" }}>
+            <img src={logo} alt="" style={{ height: "35px", verticalAlign: "top" }} />
+            <span className="h2 pt-4 m-2 text-white-50">CountOpedia</span>
         </div>
     )
 }
