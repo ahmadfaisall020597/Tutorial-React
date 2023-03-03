beb6e6e7d78257eead1913a07b5a52a77440c0e0
Ahmad  Faisal <ahmad.faisal@berca.co.id>
2023-03-03 13:26:41 +0700

    Add Contact UI

--- contact-pedia/src/index.js
+++ contact-pedia/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Footer from './Components/Layouts/Footer';
-import Header from './Components/Layouts/Header';
 import ContactIndex from './Components/ContactPages/ContactIndex';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
