e2f090089cbe1cb50abbfde57a0db1d11e2071c7
Ahmad  Faisal <ahmad.faisal@berca.co.id>
2023-03-06 10:33:41 +0700

    Add Validations

--- contact-pedia/src/Components/ContactPages/AddContact.jsx
+++ contact-pedia/src/Components/ContactPages/AddContact.jsx
@@ -14,41 +14,63 @@ class AddContact extends React.Component {
         const name = e.target.elements.contactName.value.trim();
         const email = e.target.elements.contactEmail.value.trim();
         const phone = e.target.elements.contactPhone.value.trim();
-        this.props.handleAddContact({ name: name, email: email, phone: phone });
+        const response = this.props.handleAddContact({
+            name: name,
+            email: email,
+            phone: phone
+        });
+
+        if (response.status === 'successs') {
+            this.setState({
+                errorMessage: undefined,
+                successMessage: response.msg
+            });
+            document.querySelector(".contact-form").reset();
+        } else {
+            this.setState({
+                errorMessage: response.msg,
+                successMessage: undefined
+            })
+        }
     };
 
     render() {
         return (
             <div className="border col-12 text-white p-2">
-                <div className="row p-2">
-                    <div className="col-12 text-white-50">Add a new Contact</div>
-                    <div className="col-12 col-md-4 p-1">
-                        <input
-                            className="form-control form-control-sm"
-                            placeholder="Name..."
-                            name="contactName"
-                        ></input>
+                <form
+                    onSubmit={this.handleAddContactFormSubmit}
+                    className="contact-form"
+                >
+                    <div className="row p-2">
+                        <div className="col-12 text-white-50">Add a new Contact</div>
+                        <div className="col-12 col-md-4 p-1">
+                            <input
+                                className="form-control form-control-sm"
+                                placeholder="Name..."
+                                name="contactName"
+                            ></input>
+                        </div>
+                        <div className="col-12 col-md-4 p-1">
+                            <input
+                                className="form-control form-control-sm"
+                                placeholder="Email..."
+                                name="contactEmail"
+                            ></input>
+                        </div>
+                        <div className="col-12 col-md-4 p-1">
+                            <input
+                                className="form-control form-control-sm"
+                                placeholder="Phone..."
+                                name="contactPhone"
+                            ></input>
+                        </div>
+                        <div className="col-12 col-md-6 offset-md-3 p-1">
+                            <button className="btn btn-primary btn-sm form-control">
+                                Create
+                            </button>
+                        </div>
                     </div>
-                    <div className="col-12 col-md-4 p-1">
-                        <input
-                            className="form-control form-control-sm"
-                            placeholder="Email..."
-                            name="contactEmail"
-                        ></input>
-                    </div>
-                    <div className="col-12 col-md-4 p-1">
-                        <input
-                            className="form-control form-control-sm"
-                            placeholder="Phone..."
-                            name="contactPhone"
-                        ></input>
-                    </div>
-                    <div className="col-12 col-md-6 offset-md-3 p-1">
-                        <button className="btn btn-primary btn-sm form-control">
-                            Create
-                        </button>
-                    </div>
-                </div>
+                </form>
             </div>
         )
     }
