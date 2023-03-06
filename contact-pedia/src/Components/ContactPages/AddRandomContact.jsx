fedf82c5a5edc437eb3f11a6ff5ed37ebc0bb6ee
Ahmad  Faisal <ahmad.faisal@berca.co.id>
2023-03-06 14:10:41 +0700

    Axios Call

--- contact-pedia/src/Components/ContactPages/AddRandomContact.jsx
+++ contact-pedia/src/Components/ContactPages/AddRandomContact.jsx
@@ -1,7 +1,17 @@
+import { getRandomUser } from "../../Utility/api"
+
+const getRandomContact = async () => {
+    const responseFromAPI = await getRandomUser();
+    console.log(responseFromAPI);
+}
+
 export default function AddRandomContact() {
     return (
         <div>
-            <button className="btn btn-success form-control">
+            <button
+                className="btn btn-success form-control"
+                onClick={() => getRandomContact()}
+            >
                 Add Random Contact
             </button>
         </div>
