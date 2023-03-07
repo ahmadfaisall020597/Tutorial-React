307bc6b9ef5a19ed606b7cd1a1bf10346b215abe
Ahmad  Faisal <ahmad.faisal@berca.co.id>
2023-03-07 14:56:34 +0700

    Initial Setup - Functional Component

--- cycl-pedia/src/index.js
+++ cycl-pedia/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from "./Header";
 import CyclPediaClassPage from './CyclPediaClassPage';
+import CyclPediaFuncPage from './CyclPediaFuncPage';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -12,6 +13,10 @@ root.render(
       <div className="col-6">
         <span className="h1 text-warning text-center">Class Component</span>
         <CyclPediaClassPage />
+      </div>
+      <div className="col-6">
+        <span className="h1 text-warning text-center">Func Component</span>
+        <CyclPediaFuncPage />
       </div>
     </div>
   </div>
