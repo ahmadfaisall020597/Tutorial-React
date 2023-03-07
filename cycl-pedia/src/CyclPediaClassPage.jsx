2bc9fb7d350ac92d72a9f380ba1522a00a2ec794
Ahmad  Faisal <ahmad.faisal@berca.co.id>
2023-03-07 11:29:08 +0700

    Seperating out Instructor

--- cycl-pedia/src/CyclPediaClassPage.jsx
+++ cycl-pedia/src/CyclPediaClassPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { getRandomUser } from "./Utility/Api";
+import Instructor from "./Instructor"
 
 class CyclPediaClassPage extends React.Component {
     constructor(props) {
@@ -16,19 +17,7 @@ class CyclPediaClassPage extends React.Component {
 
     componentDidMount = async () => {
         console.log("Component Did Mount");
-        // const response = await getRandomUser();
-        // console.log(response);
-        // this.setState((prevState) => {
-        //     return {
-        //         instructor: {
-        //             name: response.data.first_name + " " + response.data.last_name,
-        //             email: response.data.email,
-        //             phone: response.data.phone_number,
-        //         },
-        //     };
-        // });
         if (JSON.parse(localStorage.getItem("cylcopediaState"))) {
-            // this.setState(JSON.parse(localStorage.getItem("cylcopediaState")));
         } else {
             const response = await getRandomUser();
             console.log(response);
@@ -74,16 +63,7 @@ class CyclPediaClassPage extends React.Component {
         return (
             <div>
                 {this.state.instructor && (
-                    <div className="p-3">
-                        <span className="h4 text-success">Instructor</span>
-                        <i className=" bi bi-toggle-off btn btn-success btn-sm"></i>
-                        <br />
-                        Name: {this.state.instructor.name} <br />
-                        Email : {this.state.instructor.email}
-                        <br />
-                        Phone : {this.state.instructor.phone}
-                        <br />
-                    </div>
+                    <Instructor instructor={this.state.instructor} />
                 )}
 
                 <div className="p-3">
