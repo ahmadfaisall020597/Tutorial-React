0ce5d68c0076c3678226274615475a00b4f7ce0f
Ahmad  Faisal <ahmad.faisal@berca.co.id>
2023-03-07 09:56:33 +0700

    Display Instructor

--- cycl-pedia/src/index.js
+++ cycl-pedia/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from "./Header";
+import CyclPediaClassPage from './CyclPediaClassPage';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Header />)
+root.render(
+  <div>
+    <Header />
+    <div className="row text-white">
+      <div className="col-6">
+        <span className="h1 text-warning text-center">Class Component</span>
+        <CyclPediaClassPage />
+      </div>
+    </div>
+  </div>
+);
 
